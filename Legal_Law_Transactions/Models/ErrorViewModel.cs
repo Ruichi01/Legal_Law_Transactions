645bf73bad9162ad5f41045ccfacbb5cbc3a307e
Louis Daniel Lazarraga <louislazarraga@gmail.com>
2025-05-23 20:57:02 +0800

    updated ui

--- Legal_Law_Transactions/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Legal_Law_Transactions.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Legal_Law_Transactions/Services/IAdminLogService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Legal_Law_Transactions.Services
-{
-    public interface IAdminLogService
-    {
-        void LogAction(int adminId, string action, string target, string details);
-    }
-
-}
-    
